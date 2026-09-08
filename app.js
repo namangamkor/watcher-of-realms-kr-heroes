bcbbba05ba091aedb6c07de1f337bcce2e0f85e1
namangamkor <namangamkor@gmail.com>
2026-09-09 00:08:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -482,7 +482,7 @@ function render() {
   }
 }
 
-fetch("./heroes.json?v=2.10.15")
+fetch("./heroes.json?v=2.11.28a")
   .then((response) => {
     if (!response.ok) throw new Error("heroes.json load failed");
     return response.json();
