587e15c6d05046274b4d9bcdbf528c71fccaeffc
namangamkor <namangamkor@gmail.com>
2026-09-17 00:20:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "namangam-wor-v21304o-search-wrapper-soft";
+const CACHE_NAME = "namangam-wor-v21304q-search-wrapper-lighter";
 
 const CORE_ASSETS = [
   "/styles.css?v=2.13.4m",
